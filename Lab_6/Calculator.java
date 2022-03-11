fe0396db49340280d3b6784db24a413ff720e8cb
ToniTannous <99818685+ToniTannous@users.noreply.github.com>
2022-03-11 19:13:00 +0200

    Add files via upload

---
+++ Lab_6/Calculator.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+
+public class Calculator {
+    
+private int num1;
+    
+private int num2;
+    
+private String operator;
+private num;
+  
+// Constructor to initialize the calculator
+    
+public Calculator() {
+        
+// Get user input
+        
+Scanner scanCalc = new Scanner(System.in);
+System.out.println("Input the first number: ");
+num1 = scanCalc.nextInt();scanCalc.nextLine();
+System.out.print("Input the operator: ");
+operator = scanCalc.nextLine();
+System.out.println("Input the second number: ");
+num2 = scanCalc.nextInt();
+
+
+int num;
+switch(operator){
+case "+":
+	add(num1,num2);
+	break;
+case "-":
+	substract(num1,num2);
+	break;
+case "X":
+	multiply(num1,num2);
+case "/":
+	divide(num1,nume2);
+  default:
+           break;
+   }
+      }      
+ /**
+         * Continue code here to ask for the first number, the operator, and the second number, IN THAT ORDER
+         */
+
+       
+ // Decide on the operation to perform 
+        
+/**
+         * Insert code to decide on which function to call, based on the operator the user chose
+         * Hint: Switch statement
+         */
+    }
+
+   
+ public String add(int a, int b) {
+ 
+num1=a;
+num2=b;
+num= num1 +nume2;
+return num1 + "+" +num2+ "="+num;
+   }
+
+    
+public String subtract(int a, int b) {
+
+num1 = a;
+num2 = b;
+num =num1 =num2;
+return num1 + "-" +num2 +"="+num;
+ }
+
+   
+ public String multiply(int a, int b) {
+
+num1 = a;
+num2 = b;
+num = num1*num2;
+return num1+ "X"+num2 +"="+num;
+          }
+
+    
+public String divide(int a, int b) {
+      
+num1 =a;
+num2 =b;
+num =num1/num2;
+return num1+ "/"+num2+ "=" +num;
+
+}
+
+   public String toString(){
+     return num1 +operator + num2 +"=" +num;
+          }
+             }
---
+++ Lab_6/EntryPoint.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class EntryPoint {
+    public static void main (String[] args) {
+        
+
+
+        BasicWeek bw = new BasicWeek();
+        AdvancedWeek aw = new AdvancedWeek();
+        Calculator calc = new Calculator();
+
+
+	System.out.println(calc);
+    }
+}
